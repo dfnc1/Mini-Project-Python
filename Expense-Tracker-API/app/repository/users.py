2e726ec8fab077503b95d66e50cb6e392c0eaafb
dfnc1 <dianfnc06@gmail.com>
2026-04-21 10:56:33 +0700

    refactor: refactor: rename username user to str and add returning

--- Expense-Tracker-API/app/repository/users.py
+++ Expense-Tracker-API/app/repository/users.py
@@ -3,9 +3,8 @@ import asyncpg
 from app.schemas import UserInDB, User
 
 async def add_user(user: UserInDB, conn: asyncpg.Pool) -> User:
-    return await conn.fetchrow("INSERT INTO users (username, hashed_password) VALUES ($1, $2)",
+    return await conn.fetchrow("INSERT INTO users (username, hashed_password) VALUES ($1, $2) RETURNING username",
                                user.username, user.hashed_password)
 
-async def get_user(user: User, conn: asyncpg.Pool) -> User:
-    return await conn.fetchrow("SELECT * FROM users WHERE username = $1",
-                               user.username)
+async def get_user(username: str, conn: asyncpg.Pool) -> User:
+    return await conn.fetchrow("SELECT * FROM users WHERE username = $1", username)
