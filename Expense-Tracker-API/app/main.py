33876d2636c66fb6b65ef81cf988f858b2af34b5
dfnc1 <dianfnc06@gmail.com>
2026-05-01 18:15:10 +0700

    feat: add cors

--- Expense-Tracker-API/app/main.py
+++ Expense-Tracker-API/app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.databases import get_pool, close_pool
 from app.routers import auth, post
@@ -11,6 +12,19 @@ async def lifespan(app: FastAPI):
     yield
     await close_pool()
 app = FastAPI(lifespan=lifespan)
+
+origins = [
+    "https://your-domain.com",
+    "http://your-localhost:8080"
+]
+
+app.add_middleware(
+    CORSMiddleware, # type: ignore
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(auth.router)
 app.include_router(post.router)
